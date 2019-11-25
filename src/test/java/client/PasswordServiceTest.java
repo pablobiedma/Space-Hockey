3b655410aeb7acccf799085e6114145aa4ef0294
Maciej <m.styczen@student.tudelft.nl>
2019-11-25 13:45:01 +0100

    Finished password service class - will be used for authentication

--- src/test/java/client/PasswordServiceTest.java
+++ src/test/java/client/PasswordServiceTest.java
@@ -2,11 +2,13 @@ package client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordServiceTest {
     private String password = "password";
-    private String hash = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+    private String hash = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=";
     @Test
     void hashTest() {
         assertEquals(hash, PasswordService.hashPassword(password));
