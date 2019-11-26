2d4220cf67d48d4a98c141e64c80233dcf24f1fa
Pablo Biedma <P.BiedmaNunez@student.tudelft.nl>
2019-11-26 15:54:23 +0100

    removed unnecesary buttons

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -60,14 +60,14 @@ public class Game extends ApplicationAdapter {
 
 		Skin mySkin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 
-		Label title = new Label("Buttons with Skins",mySkin,"big-black");
-		title.setSize(Gdx.graphics.getWidth(),row_height*2);
-		title.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
-		title.setAlignment(Align.center);
-		stage.addActor(title);
+//		//Label title = new Label("Buttons with Skins",mySkin,"big-black");
+//		title.setSize(Gdx.graphics.getWidth(),row_height*2);
+//		title.setPosition(0,Gdx.graphics.getHeight()-row_height*2);
+//		title.setAlignment(Align.center);
+//		stage.addActor(title);
 
 		// Button
-		Button button1 = new Button(mySkin,"small");
+		Button button1 = new Button(mySkin);
 		button1.setSize(col_width*4,row_height);
 		button1.setPosition(col_width,Gdx.graphics.getHeight()-row_height*3);
 		button1.addListener(new InputListener(){
@@ -84,7 +84,7 @@ public class Game extends ApplicationAdapter {
 		stage.addActor(button1);
 
 		// Text Button
-		Button button2 = new TextButton("Text Button",mySkin,"small");
+		Button button2 = new TextButton("Text Button",mySkin);
 		button2.setSize(col_width*4,row_height);
 		button2.setPosition(col_width*7,Gdx.graphics.getHeight()-row_height*3);
 		button2.addListener(new InputListener(){
@@ -100,49 +100,48 @@ public class Game extends ApplicationAdapter {
 		});
 		stage.addActor(button2);
 
-		// ImageButton
-		ImageButton button3 = new ImageButton(mySkin);
-		button3.setSize(col_width*4,(float)(row_height*2));
-		button3.getStyle().imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_off.png"))));
-		button3.getStyle().imageDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_on.png"))));
-		button3.setPosition(col_width,Gdx.graphics.getHeight()-row_height*6);
-		button3.addListener(new InputListener(){
-			@Override
-			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-				outputLabel.setText("Press a Button");
-			}
-			@Override
-			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-				outputLabel.setText("Pressed Image Button");
-				return true;
-			}
-		});
-		stage.addActor(button3);
+//		// ImageButton
+//		ImageButton button3 = new ImageButton(mySkin);
+//		button3.setSize(col_width*4,(float)(row_height*2));
+//		button3.getStyle().imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_off.png"))));
+//		button3.getStyle().imageDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_on.png"))));
+//		button3.setPosition(col_width,Gdx.graphics.getHeight()-row_height*6);
+//		button3.addListener(new InputListener(){
+//			@Override
+//			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//				outputLabel.setText("Press a Button");
+//			}
+//			@Override
+//			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+//				outputLabel.setText("Pressed Image Button");
+//				return true;
+//			}});
+		//stage.addActor(button3);
 
-		//ImageTextButton
-		ImageTextButton button4 = new ImageTextButton("ImageText Btn",mySkin,"small");
-		button4.setSize(col_width*4,(float)(row_height*2));
-		button4.getStyle().imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_off.png"))));
-		button4.getStyle().imageDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_on.png"))));
-		button4.setPosition(col_width*7,Gdx.graphics.getHeight()-row_height*6);
-		button4.addListener(new InputListener(){
-			@Override
-			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-				outputLabel.setText("Press a Button");
-			}
-			@Override
-			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-				outputLabel.setText("Pressed Image Text Button");
-				return true;
-			}
-		});
-		stage.addActor(button4);
-
-		outputLabel = new Label("Press a Button",mySkin,"black");
-		outputLabel.setSize(Gdx.graphics.getWidth(),row_height);
-		outputLabel.setPosition(0,row_height);
-		outputLabel.setAlignment(Align.center);
-		stage.addActor(outputLabel);
+//		//ImageTextButton
+//		ImageTextButton button4 = new ImageTextButton("ImageText Btn",mySkin,"small");
+//		button4.setSize(col_width*4,(float)(row_height*2));
+//		button4.getStyle().imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_off.png"))));
+//		button4.getStyle().imageDown = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("switch_on.png"))));
+//		button4.setPosition(col_width*7,Gdx.graphics.getHeight()-row_height*6);
+//		button4.addListener(new InputListener(){
+//			@Override
+//			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+//				outputLabel.setText("Press a Button");
+//			}
+//			@Override
+//			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+//				outputLabel.setText("Pressed Image Text Button");
+//				return true;
+//			}
+//		});
+//		stage.addActor(button4);
+//
+//		outputLabel = new Label("Press a Button",mySkin,"black");
+//		outputLabel.setSize(Gdx.graphics.getWidth(),row_height);
+//		outputLabel.setPosition(0,row_height);
+//		outputLabel.setAlignment(Align.center);
+//		stage.addActor(outputLabel);
 	}
 
 	@Override
