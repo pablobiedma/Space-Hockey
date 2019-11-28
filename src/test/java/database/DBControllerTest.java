45c36310cc27fed4758ce8f17401c36d099667da
Maciej <m.styczen@student.tudelft.nl>
2019-11-28 11:18:48 +0100

    Fixed db controller class - supports basic interaction with database. Dummy test for the controler - to be redone with mocks.

--- src/test/java/database/DBControllerTest.java
+++ src/test/java/database/DBControllerTest.java
@@ -9,23 +9,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 //BIG TO-DO -> create mock tests, these tests here are useless in the long run//
 class DBControllerTest {
-//    @Test
-//    void userExistsTest() throws SQLException {
-//        Connection connection = DBController.setUpConnection();
-//        assertTrue(DBController.userExists("mstyczen"));
-//        assertFalse(DBController.userExists("hello"));
-//    }
-//
-//    @Test
-//    public void getPasswordHashTest() throws SQLException {
-//        assertEquals("XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg", DBController.getHashedPassword("mstyczen"));
-//    }
-//
-//    @Test
-//    public void createUserTest() throws SQLException {
-//        String username = "test";
-//        String hashedPassword = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg";
-//        DBController.createUser(username, hashedPassword);
-//        assertTrue(DBController.userExists(username));
-//    }
+    @Test
+    void userExistsTest() throws SQLException {
+        assertTrue(DBController.userExists("mstyczen"));
+        assertFalse(DBController.userExists("hello"));
+    }
+
+    @Test
+    public void getPasswordHashTest() throws SQLException {
+        assertEquals("XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg", DBController.getHashedPassword("mstyczen"));
+    }
+
+    @Test
+    public void createUserTest() throws SQLException {
+        String username = "test";
+        String hashedPassword = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg";
+        DBController.createUser(username, hashedPassword);
+        assertTrue(DBController.userExists(username));
+    }
 }
