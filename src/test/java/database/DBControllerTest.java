a00fda51bcfb3d8a7180930340c7907ec21c33e0
Maciej <m.styczen@student.tudelft.nl>
2019-11-28 16:43:28 +0100

    Changes in DBController and DBController test - connection is set up in a seperate method and I got rid of some throws annotations

--- src/test/java/database/DBControllerTest.java
+++ src/test/java/database/DBControllerTest.java
@@ -1,31 +1,40 @@
 package database;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import org.mockito.Mockito.*;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//BIG TO-DO -> create mock tests, these tests here are useless in the long run//
 class DBControllerTest {
+    private transient DBController database;
+    @BeforeEach
+    void setupTestEnvironment() {
+        database = new DBController();
+    }
+
+    @Test
+    void constructorTest() {
+        assertNotNull(database);
+    }
 
     @Test
     void userExistsTest() throws SQLException {
-        assertFalse(DBController.userExists("----------"));
-        assertTrue(DBController.userExists("hello"));
+        assertFalse(database.userExists("----------"));
+        assertTrue(database.userExists("hello"));
     }
 
     @Test
     public void getPasswordHashTest() throws SQLException {
-        assertEquals("LPJNul+wow4m6DsqxbninhsWHlwfp0JecwQzYpOLmCQ=", DBController.getHashedPassword("hello"));
+        assertEquals("LPJNul+wow4m6DsqxbninhsWHlwfp0JecwQzYpOLmCQ=",
+                database.getHashedPassword("hello"));
     }
 
     @Test
     public void createUserTest() throws SQLException {
         String username = "test";
         String hashedPassword = "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg";
-        DBController.createUser(username, hashedPassword);
-        assertTrue(DBController.userExists(username));
+        database.createUser(username, hashedPassword);
+        assertTrue(database.userExists(username));
     }
 }
