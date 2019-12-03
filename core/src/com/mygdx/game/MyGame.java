b6b080c22ba8fed908a4e23ee1b5d57262482f74
Maciej <m.styczen@student.tudelft.nl>
2019-12-03 23:09:57 +0100

    Fixing a conflct.

--- core/src/com/mygdx/game/MyGame.java
+++ core/src/com/mygdx/game/MyGame.java
@@ -1,27 +1,8 @@
 package com.mygdx.game;
 
-<<<<<<< HEAD
-//import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL20;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//import com.badlogic.gdx.graphics.g2d.TextureRegion;
-//import com.badlogic.gdx.scenes.scene2d.InputEvent;
-//import com.badlogic.gdx.scenes.scene2d.InputListener;
-//import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
-//import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-//import com.badlogic.gdx.utils.Align;
-//import com.badlogic.gdx.utils.Scaling;
-//import com.badlogic.gdx.utils.viewport.ScreenViewport;
-//import com.mygdx.game.screens.GameScreen;
-=======
-import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
->>>>>>> Added a lot of game functionallity - collisions, paddle movement, puck movement, textures.
 import com.mygdx.game.screens.MenuScreen;
 
 public class MyGame extends com.badlogic.gdx.Game {
