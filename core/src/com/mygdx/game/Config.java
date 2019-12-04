ce5543589e51cdb65b9a32332b440087cc45e660
Maciej <m.styczen@student.tudelft.nl>
2019-12-04 11:51:03 +0100

    Changed organisation of config file. Added a line for speed of the paddles.

--- core/src/com/mygdx/game/Config.java
+++ core/src/com/mygdx/game/Config.java
@@ -4,23 +4,39 @@ import com.badlogic.gdx.Input;
 import com.mygdx.game.movement.KeyCodeSet;
 
 public final class Config {
+    /**
+     * Resolution and viewport parameters.
+     */
     public static final float VIEWPORT_SIZE = 50;
+    public static final float RESOLUTION = 1000;
 
-    public static final float PADDLE_RADIUS = 1;
+    /**
+     * Paddle parameters.
+     */
+    public static final float PADDLE_RADIUS = 1.5f;
     public static final float PADDLE_DENSITY = 0.5f;
     public static final float PADDLE_FRICTION = 0.1f;
     public static final float PADDLE_RESTITUTION = 0.1f;
 
-    public static final float PUCK_RADIUS = 0.75f;
-    public static final float PUCK_DENSITY = 0.5f;
+    /**
+     * Puck parameters.
+     */
+    public static final float PUCK_RADIUS = 1f;
+    public static final float PUCK_DENSITY = 0.3f;
     public static final float PUCK_FRICTION = 0.05f;
     public static final float PUCK_RESTITUTION = 0.9f;
+    public static final float PADDLE_SPPED = 15;
 
+    /**
+     * Wall parameters.
+     */
     public static final float WALL_HEIGHT = 10;
     public static final float WALL_WIDTH = 20;
     public static final float WALL_THICKNESS = 0.2f;
 
+    /**
+     * Key code sets for paddles (arrows for the right one, wasd for the left one).
+     */
     public static final KeyCodeSet BLUE_PADDLE_KEYS = new KeyCodeSet(Input.Keys.DPAD_LEFT, Input.Keys.DPAD_RIGHT, Input.Keys.DPAD_UP, Input.Keys.DPAD_DOWN);
     public static final KeyCodeSet RED_PADDLE_KEYS = new KeyCodeSet(Input.Keys.A, Input.Keys.D, Input.Keys.W, Input.Keys.S);
-    public static final float RESOLUTION = 1000;
 }
