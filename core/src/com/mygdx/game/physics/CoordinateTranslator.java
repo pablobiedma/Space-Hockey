6bf267affe5c65676d9efcec2ebe08b7dad977a6
Maciej <m.styczen@student.tudelft.nl>
2019-12-04 11:50:39 +0100

    Moved translating coordnitaes to pixels into a seperate class.

---
+++ core/src/com/mygdx/game/physics/CoordinateTranslator.java
@@ -0,0 +1,29 @@
+package com.mygdx.game.physics;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.mygdx.game.Config;
+
+/**
+ * Class used to translate Viewport coordinates to pixel coordinates. Needed for positioning the sprites.
+ */
+public class CoordinateTranslator {
+    /**
+     * Translates coordinate x.
+     * @param s sprite for which the translation is performed.
+     * @param x coordinate.
+     * @return resulting coordinate.
+     */
+    public static float translateX(Sprite s, float x) {
+        return Config.RESOLUTION / 2 + x / Config.VIEWPORT_SIZE * Config.RESOLUTION - s.getWidth() / 2;
+    }
+
+    /**
+     * Translates coordinate y.
+     * @param s sprite for which the translation is performed.
+     * @param y coordinate.
+     * @return resulting coordinate.
+     */
+    public static float translateY(Sprite s, float y) {
+        return Config.RESOLUTION / 2 + y / Config.VIEWPORT_SIZE * Config.RESOLUTION - s.getHeight() / 2;
+    }
+}
