c129c1dd87f876767e403b54e853bf0b4277e4af
Maciej <m.styczen@student.tudelft.nl>
2019-12-05 22:01:10 +0100

    Created a game screen class. Distributed the functionallity that was before in game screen among the game operator class and all the game elements classes.

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -4,156 +4,33 @@ import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.mygdx.game.physics.Config;
-import com.mygdx.game.movement.MovementController;
-import com.mygdx.game.physics.CoordinateTranslator;
+import com.mygdx.game.game_backend.Config;
+import com.mygdx.game.game_backend.GameOperator;
 
 /**
  * Game screen class - implements the game screen functionality.
  */
 public class GameScreen extends ApplicationAdapter implements Screen {
+    Game game;
+    GameOperator gameOperator;
     SpriteBatch batch;
-    Sprite redPaddleSprite;
-    Sprite bluePaddleSprite;
-    Sprite puckSprite;
-    Sprite pitchSprite;
-    Body redPaddleBody;
-    Body bluePaddleBody;
-    Body puckBody;
-    MovementController bluePaddleController;
-    MovementController redPaddleController;
     World world;
     Box2DDebugRenderer debugRenderer;
     Camera camera;
-    Stage stage;
 
-    public GameScreen() {
+    /**
+     * Constructor for game screen class. Creates the game screen object.
+     */
+    public GameScreen(Game game) {
+        this.game = game;
         Box2D.init();
         world = new World(new Vector2(0, 0), true);
-
-        initializeSprites();
-        initializeBodies(Config.VIEWPORT_SIZE);
-        initializeFixtures();
-        initializeWalls(Config.WALL_WIDTH, Config.WALL_HEIGHT, Config.WALL_THICKNESS);
-        initializeMovementControllers();
-
+        gameOperator = new GameOperator(world);
         debugRenderer = new Box2DDebugRenderer();
         camera = new OrthographicCamera(Config.VIEWPORT_SIZE, Config.VIEWPORT_SIZE);
-    }
-
-    private void initializeMovementControllers() {
-        bluePaddleController = new MovementController(bluePaddleBody, Config.BLUE_PADDLE_KEYS, false);
-        redPaddleController = new MovementController(redPaddleBody, Config.RED_PADDLE_KEYS, true);
-    }
-
-    private void initializeFixtures() {
-
-        //initialize puck and paddles
-        FixtureDef paddleFixtureDef = getFixtureDef(new CircleShape(), Config.PADDLE_RADIUS,
-                Config.PADDLE_DENSITY, Config.PADDLE_FRICTION, Config.PADDLE_RESTITUTION);
-        FixtureDef puckFixtureDef = getFixtureDef(new CircleShape(), Config.PUCK_RADIUS,
-                Config.PUCK_DENSITY, Config.PUCK_FRICTION, Config.PUCK_RESTITUTION);
-
-        redPaddleBody.createFixture(paddleFixtureDef);
-        bluePaddleBody.createFixture(paddleFixtureDef);
-        puckBody.createFixture(puckFixtureDef);
-    }
-
-    private FixtureDef getFixtureDef(CircleShape shape, float radius, float density, float friction, float restitution) {
-        shape.setRadius(radius);
-
-        FixtureDef res = new FixtureDef();
-        res.shape = shape;
-        res.density = density;
-        res.friction = friction;
-        res.restitution = restitution;
-        shape.dispose();
-        return res;
-    }
-
-    public void initializeBodies(float viewportSize) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-        bodyDef.position.set(0, 0);
-        puckBody  = world.createBody(bodyDef);
-
-        bodyDef.position.set(viewportSize / 4, 0);
-        bluePaddleBody  = world.createBody(bodyDef);
-
-        bodyDef.position.set(-viewportSize / 4, 0);
-        redPaddleBody = world.createBody(bodyDef);
-    }
-
-    /**
-     * Initializes walls.
-     */
-    private void initializeWalls(float width, float height, float thickness) {
-        initializeWall(0, -height,
-                width, thickness);
-        initializeWall(0, height,
-                width, thickness);
-        initializeWall(width, 0,
-                thickness, height);
-        initializeWall(-width, 0,
-                thickness, height);
-    }
-
-    /**
-     * Initializes a wall.
-     * @param x position of the centre of the wall.
-     * @param y position of the centre of the wall.
-     * @param width of the wall.
-     * @param height of the wall.
-     */
-    private void initializeWall(float x, float y, float width, float height) {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.StaticBody;
-        bodyDef.position.set(x, y);
-        Body wallBody  = world.createBody(bodyDef);
-
-        PolygonShape wall = new PolygonShape();
-        wall.setAsBox(width, height);
-        wallBody.createFixture(wall, 0);
-    }
-
-
-    /**
-     * Initializes the sprites.
-     */
-    private void initializeSprites() {
-        Texture bluePaddleTexture = new Texture("sprite/blue-paddle.png");
-        bluePaddleSprite = new Sprite(bluePaddleTexture);
-        float paddleSize = 2 * Config.PADDLE_RADIUS / Config.VIEWPORT_SIZE * Config.RESOLUTION;
-        bluePaddleSprite.setSize(paddleSize, paddleSize);
-
-        debugRenderer = new Box2DDebugRenderer();
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-
-        Texture redPaddleTexture = new Texture("sprite/red-paddle.png");
-        redPaddleSprite = new Sprite(redPaddleTexture);
-        redPaddleSprite.setSize(paddleSize, paddleSize);
-
-        Texture puckTexture = new Texture("sprite/puck.png");
-        puckSprite = new Sprite(puckTexture);
-        float puckSize = 2 * Config.PUCK_RADIUS / Config.VIEWPORT_SIZE * Config.RESOLUTION;
-        puckSprite.setSize(puckSize, puckSize);
-
-        Texture pitchTexture = new Texture("sprite/pitch.png");
-        pitchSprite = new Sprite(pitchTexture);
-        float pitchWidth = 2 * Config.WALL_WIDTH / Config.VIEWPORT_SIZE * Config.RESOLUTION;
-        float pitchHeight = 2 * Config.WALL_HEIGHT / Config.VIEWPORT_SIZE * Config.RESOLUTION;
-
-        pitchSprite.setSize(pitchWidth, pitchHeight);
-        pitchSprite.setPosition(
-                CoordinateTranslator.translateX(pitchSprite, 0),
-                CoordinateTranslator.translateY(pitchSprite, 0));
     }
 
     @Override
@@ -161,7 +38,6 @@ public class GameScreen extends ApplicationAdapter implements Screen {
      * Shows the view.
      */
     public void show() {
-        Gdx.input.setInputProcessor(stage);
         batch = new SpriteBatch();
     }
 
@@ -176,33 +52,9 @@ public class GameScreen extends ApplicationAdapter implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        drawSprites();
-
-        bluePaddleController.updateVelocity();
-        redPaddleController.updateVelocity();
+        gameOperator.drawSprites(batch);
+        gameOperator.updatePhysics();
         debugRenderer.render(world, camera.combined);
-    }
-
-    private void drawSprites() {
-        batch.begin();
-        pitchSprite.draw(batch);
-
-        puckSprite.setPosition(
-                CoordinateTranslator.translateX(puckSprite, puckBody.getPosition().x),
-                CoordinateTranslator.translateY(puckSprite, puckBody.getPosition().y));
-        puckSprite.draw(batch);
-
-        redPaddleSprite.setPosition(
-                CoordinateTranslator.translateX(redPaddleSprite, redPaddleBody.getPosition().x),
-                CoordinateTranslator.translateY(redPaddleSprite, redPaddleBody.getPosition().y));
-        redPaddleSprite.draw(batch);
-
-        bluePaddleSprite.setPosition(
-                CoordinateTranslator.translateX(bluePaddleSprite, bluePaddleBody.getPosition().x),
-                CoordinateTranslator.translateY(bluePaddleSprite, bluePaddleBody.getPosition().y));
-        bluePaddleSprite.draw(batch);
-
-        batch.end();
     }
 
     @Override
