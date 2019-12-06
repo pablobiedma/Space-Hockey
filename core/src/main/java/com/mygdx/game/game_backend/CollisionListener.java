b31a1d2db78761ada977cab8d42c54abe0e40fc3
Jurriaan Buitenweg <j.r.buitenweg@student.tudelft.nl>
2019-12-06 14:06:51 +0100

    Add goal scoring

---
+++ core/src/main/java/com/mygdx/game/game_backend/CollisionListener.java
@@ -0,0 +1,50 @@
+package com.mygdx.game.physics;
+
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class CollisionListener implements ContactListener {
+    private int scoreA;
+    private int scoreB;
+
+    @Override
+    public void beginContact(Contact contact) {
+        System.out.println("Collision");
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+
+    public int getScoreA() {
+        return scoreA;
+    }
+
+    public void setScoreA(int scoreA) {
+        this.scoreA = scoreA;
+    }
+
+    public int getScoreB() {
+        return scoreB;
+    }
+
+    public void setScoreB(int scoreB) {
+        this.scoreB = scoreB;
+    }
+
+}
