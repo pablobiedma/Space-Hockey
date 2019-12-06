b83481b0c30178adf45964ddf607c6e520d5462a
Maciej <m.styczen@student.tudelft.nl>
2019-12-06 09:46:29 +0100

    Set up testing environment for the game. Fixed some of the gradle issues, wrote some simple test, generated the rest with empty methods.

---
+++ core/src/test/java/com/mygdx/game/game_elements/DynamicObjectTest.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.game_elements;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynamicObjectTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void getFixtureDef() {
+    }
+}
---
+++ core/src/test/java/com/mygdx/game/game_elements/PaddleTest.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.game_elements;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaddleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void updateVelocity() {
+    }
+}
